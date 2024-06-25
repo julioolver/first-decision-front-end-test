2270ff5d2a71370428e0e39c3be25f3d46dd0469
Julio Cesar <julioghost1@gmail.com>
2024-06-25 15:37:28 -0300

    feat #1: adjusted axios plugin

--- src/plugins/axios/index.ts
+++ src/plugins/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://some-domain.com/api/',
+    baseURL: 'http://localhost:8088/api/',
     timeout: 1000,
     headers: { 'X-Custom-Header': 'foobar' }
 });
