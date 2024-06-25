5c64e2ee3bd7452d26aa6cd668af7c3562664c89
Julio Cesar <julioghost1@gmail.com>
2024-06-25 15:36:49 -0300

    feat #1: created controller to work and call methods in back-end

--- src/module/user/controller/userController.ts
+++ src/module/user/controller/userController.ts
@@ -1,20 +1,81 @@
+import { reactive } from 'vue';
+import axios from '@/plugins/axios';
+import { UserType } from '../types/UserType';
+
+
 class UserController {
-    async createUser(user: any): Promise<void> {
-        console.log('Creating user:', user);
-    }
+  user = reactive<UserType>({
+    id: null,
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+  });
 
-    async updateUser(user: any): Promise<void> {
-        console.log('Updating user:', user);
-    }
+  snackbar = reactive({
+    show: false,
+    text: '',
+    color: 'green'
+  })
 
-    async deleteUser(userId: string): Promise<void> {
-        console.log('Deleting user with ID:', userId);
-    }
+  headers = [
+    { title: "Código", key: "id" },
+    { title: "Nome", key: "name" },
+    { title: "E-mail", key: "email" },
+    { title: "Ações", key: "actions", sortable: false },
+  ];
 
-    async getAllUsers(): Promise<any[]> {
-        console.log('Getting all users');
-        return [];
+  async createUser() {
+    try {
+      const response = await axios.post('/users', this.user);
+      return response.data;
+    } catch (error: any) {
+      if (error.response && error.response.status === 422) {
+        console.error('Validation errors:', error.response.data.errors);
+
+        this.snackbar.show = true;
+        this.snackbar.text = Object.values(error.response.data.errors).flat().join("<br>");
+        this.snackbar.color = 'red'
+
+        return error.response.data.errors;
+      }
+      console.error('Error creating user:', error);
     }
+  }
+
+  async updateUser() {
+    try {
+      const response = await axios.put(`/users/${this.user.id}`, this.user);
+
+      this.snackbar.color = 'green';
+      this.snackbar.show = true;
+      this.snackbar.text = 'Usuário atualizado com sucesso!';
+      return response.data;
+    } catch (error) {
+      console.error('Error updating user:', error);
+    }
+  }
+
+  async deleteUser(userId: number) {
+    try {
+      const response = await axios.delete(`/users/${userId}`);
+      console.log('User deleted:', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error deleting user:', error);
+    }
+  }
+
+  async getAllUsers() {
+    try {
+      const response = await axios.get('/users');
+
+      return response.data.data;
+    } catch (error) {
+      console.error('Error getting users:', error);
+      return [];
+    }
+  }
 }
 
 export { UserController };
